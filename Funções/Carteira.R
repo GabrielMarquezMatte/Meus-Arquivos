c12118427e910ea11440805adf86667bd6bdab98
Gabriel Matte <gabrielandremarquez.matte@gmail.com>
2021-01-13 15:28:36 -0300

    Add files via upload

--- Funções/Carteira.R
+++ Funções/Carteira.R
@@ -1,39 +1,76 @@
 carteira <- function(data.framee){
   #data.frame(symbol,date,price,n_acoes,moeda = NA ou moeda de preferência)
   options(warn = -1)
-  pacotes <- c("tidyverse","tidyquant","bizdays")
+  pacotes <- c("tidyverse","tidyquant","bizdays","GetTDData","rbcb")
   for(i in pacotes){
     suppressPackageStartupMessages(require(i,character.only = T))
   }
-  valores <- tq_get(dplyr::first(data.framee$symbol), 
-                    from = dplyr::first(data.framee$date)-5)
-  dividendos <- tq_get(dplyr::first(data.framee$symbol), 
-                       from = dplyr::first(data.framee$date)-5,
-                       get = "dividends")
-  if(!is.na(data.framee$moeda)){
-    moeda <- tq_get(dplyr::first(data.framee$moeda),
-                    from = dplyr::first(data.framee$date)-5) %>%
-      select(date,close)
-  }else{
-    moeda <- data.frame(date = valores$date,
-                        moeda = 1)
+  if(is.null(data.framee$moeda)){
+    data.framee$moeda <- NA
   }
-  colnames(moeda) <- c("date","moeda")
-  if(is.null(dim(dividendos))| any(dim(dividendos) == 0)){
-    dividendos <- data.frame(symbol = data.framee$symbol,
-                             date = data.framee$date,
-                             value = 0,
-                             price = data.framee$price,
-                             n_acoes = data.framee$n_acoes)
-    val <- valores %>%
-      left_join(dividendos, by = c("symbol","date")) %>%
-      left_join(moeda,"date")
-  }else{
+  if(is.null(data.framee$vencimento)){
+    data.framee$vencimento <- NA
+  }
+  if(is.null(data.framee$benchmark)){
+    data.framee$benchmark <- NA
+  }
+  data.framee$vencimento <- as.Date(data.framee$vencimento)
+  data.framee$date <- as.Date(data.framee$date)
+  dataa <- data.framee[1,]
+  if(!(dataa$symbol %in% c("NTN-B","NTN-B Principal","LTN","LFT","NTN-F","CDI"))){
+    valores <- tq_get(dplyr::first(data.framee$symbol), 
+                      from = dplyr::first(data.framee$date)-5)
+    dividendos <- tq_get(dplyr::first(data.framee$symbol), 
+                         from = dplyr::first(data.framee$date)-5,
+                         get = "dividends")
+    if(!is.na(data.framee$moeda)){
+      moeda <- tq_get(dplyr::first(data.framee$moeda),
+                      from = dplyr::first(data.framee$date)-5) %>%
+        select(date,close)
+    }else{
+      moeda <- data.frame(date = valores$date,
+                          moeda = 1)
+    }
+    colnames(moeda) <- c("date","moeda")
+    if(is.null(dim(dividendos))| any(dim(dividendos) == 0)){
+      dividendos <- data.frame(symbol = data.framee$symbol,
+                               date = data.framee$date,
+                               value = 0,
+                               price = data.framee$price,
+                               n_acoes = data.framee$n_acoes)
+      val <- valores %>%
+        left_join(dividendos, by = c("symbol","date")) %>%
+        left_join(moeda,"date")
+    }else{
+      val <- valores %>%
+        left_join(data.framee %>%
+                    select(symbol,date,price,n_acoes),by = c("symbol","date")) %>%
+        left_join(dividendos, by = c("symbol","date")) %>%
+        left_join(moeda,"date")
+    }
+  }else if(dataa$symbol %in% c("NTN-B","NTN-B Principal","LTN","LFT","NTN-F")){
+    download.TD.data(asset.codes = dataa$symbol)
+    valores <- read.TD.files(asset.codes = dataa$symbol,
+                             maturity = format(dataa$vencimento,"%d%m%y")) %>%
+      filter(ref.date >= dataa$date-2) %>%
+      select(ref.date,price.bid)
+    colnames(valores) <- c("date","close")
     val <- valores %>%
       left_join(data.framee %>%
-                  select(symbol,date,price,n_acoes),by = c("symbol","date")) %>%
-      left_join(dividendos, by = c("symbol","date")) %>%
-      left_join(moeda,"date")
+                  select(symbol, date, price, n_acoes,moeda), "date") %>%
+      mutate(value = 0,
+             moeda = 1,
+             symbol = dataa$symbol)
+  }
+  if(dataa$symbol == "CDI"){
+    valores <- get_series(c(CDI = 12), start_date = dataa$date) %>%
+      summarise(date,close = cumprod((CDI/100)*dataa$benchmark+1)*1000)
+    val <- valores %>%
+      left_join(data.framee %>%
+                  select(symbol, date, price, n_acoes,moeda), "date") %>%
+      mutate(value = 0,
+             moeda = 1,
+             symbol = dataa$symbol)
   }
   val <- val %>%
     mutate(value = na.fill(value,0),
@@ -42,7 +79,7 @@ carteira <- function(data.framee){
            close = close*moeda,
            aporte = price*n_acoes*moeda,
            aporte = na.fill(aporte,0),
-           valor_tot = cumsum(n_acoes)*close+cumsum(value)*cumsum(n_acoes),
+           valor_tot = cumsum(n_acoes)*close,
            price = NULL,
            moeda = NULL) %>%
     select(symbol,date,close,n_acoes,value,aporte,valor_tot)
